49eab11e6d925558ab073316724d905751670abc
amader4269 <a0987268288@gmail.com>
2026-06-14 16:14:19 +0800

    更新 repo 名稱為 cunren-association，網址改為 /cunren-association/

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 // 部署到 GitHub Pages 時，網站會位於 https://<帳號>.github.io/<repo名稱>/
-// 目前 repo 名稱為 "-"，網站會在 https://amader4269.github.io/-/
-const REPO_NAME = "-";
+// 目前 repo 名稱為 "cunren-association"，網站會在 https://amader4269.github.io/cunren-association/
+const REPO_NAME = "cunren-association";
 const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
